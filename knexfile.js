43cd741448bf0feae6f5594f7dcffb23a2aeea6b
Felipe José de Lima <lplpfwr@gmail.com>
2025-03-10 19:55:25 -0300

    last deploy

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,29 @@
-const path = require('path');
+require('dotenv').config();
 
 module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
+      filename: './src/database/database.sqlite',
     },
     migrations: {
-      directory: path.resolve(__dirname, 'src', 'database', 'knex', 'migrations'),
+      directory: './src/database/knex/migrations',
+    },
+    seeds: {
+      directory: './src/database/knex/seeds', // Caminho para as seeds no ambiente de desenvolvimento
     },
     useNullAsDefault: true,
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL, // Pegando do ambiente
+    migrations: {
+      directory: './src/database/knex/migrations',
+    },
+    seeds: {
+      directory: './src/database/knex/seeds', // Caminho para as seeds no ambiente de produção
+    },
+    ssl: { rejectUnauthorized: false }, // Importante para Render
   },
 };
